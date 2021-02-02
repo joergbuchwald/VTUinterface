a1036e1de2315b88400a91e72eb81ae20089a77e
joergbuchwald <joerg.buchwald@ufz.de>
2021-02-03 00:07:20 +0100

    adaptations for PyPI publication

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
-"""VTUinterface: a python and julia PVD/VTU API"""
+"""VTUinterface: a python PVD/VTU API"""
 
 from setuptools import setup, find_packages
 
 setup(name="VTUinterface",
-      version=0.62,
+      version=0.63,
       maintainer="Jörg Buchwald",
       maintainer_email="joerg_buchwald@ufz.de",
       author="Jörg Buchwald",
       author_email="joerg.buchwald@ufz.de",
-      url="https://github.com/joergbuchwald/VTUinterface.jl",
+      url="https://github.com/joergbuchwald/VTUinterface",
+      classifiers=["Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Visualization",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8"],
       license="MIT -  see LICENSE.txt",
       platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
       include_package_data=True,
       install_requires=["lxml", "vtk", "pandas", "scipy"],
-      py_modules=["vtuIO"])
+      py_modules=["vtuIO"],
+      package_dir={'': 'src'})
