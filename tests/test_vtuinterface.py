2c8d19d6b3f9262fe44a881bbff241f6bb270484
joergbuchwald <joerg.buchwald@ufz.de>
2021-05-24 14:53:13 +0200

    fix path in tests

--- tests/test_vtuinterface.py
+++ tests/test_vtuinterface.py
@@ -5,7 +5,7 @@ from context import vtuIO
 class TestiOGS(unittest.TestCase):
 
     def test_pvd_read(self):
-        pvdfile=vtuIO.PVDIO("../examples", "square_1e2_pcs_0.pvd", dim=2)
+        pvdfile=vtuIO.PVDIO("examples/square_1e2_pcs_0.pvd", dim=2)
         time = pvdfile.timesteps
         selected_points = {'pt0': (0.25, 0.5, 0.0), 'pt1': (0.75, 0.5, 0.0)}
         pressure_interpolation = pvdfile.read_time_series('pressure', selected_points)
@@ -14,7 +14,7 @@ class TestiOGS(unittest.TestCase):
 
     def test_point_set_read(self):
         t = 0.5
-        pvdfile=vtuIO.PVDIO("../examples", "square_1e2_pcs_0.pvd", dim=2)
+        pvdfile=vtuIO.PVDIO("examples/square_1e2_pcs_0.pvd", dim=2)
         xaxis =  [(i,0,0) for i in np.linspace(start=0.0, stop=1.0, num=100)]
         y_pred = np.linspace(start=0.5, stop=-0.5, num=100)
         pressure_xaxis_t1 = pvdfile.read_point_set_data(t, 'pressure', pointsetarray=xaxis)
@@ -22,7 +22,7 @@ class TestiOGS(unittest.TestCase):
             self.assertAlmostEqual(y_pred[i],p)
 
     def test_vtu_write_read(self):
-        vtufile = vtuIO.VTUIO("../examples/square_1e2_pcs_0_ts_1_t_1.000000.vtu", dim=2)
+        vtufile = vtuIO.VTUIO("examples/square_1e2_pcs_0_ts_1_t_1.000000.vtu", dim=2)
         def fct(x,y,z):
             return x*10
         def fct2(x,y,z):
