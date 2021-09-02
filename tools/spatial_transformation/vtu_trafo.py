24b1323dc7aad0d18070d6a4d227a953919766f8
joergbuchwald <joerg.buchwald@ufz.de>
2021-09-02 12:01:58 +0200

    removing trailing whitespaces

--- tools/spatial_transformation/vtu_trafo.py
+++ tools/spatial_transformation/vtu_trafo.py
@@ -5,16 +5,16 @@ Transforms a mesh (points and vectorial/tensorial data) to new coordinates.
 A frequent use case are 2D-slices in 3D to be transformed into x-y-coordinates
 (pre-processing), running OGS and transforming back into x-y-z coordinates
 (post-processing).
-    
+
 AUTOMATIC DETECTION
     runfile('vtu_trafo.py', args='-i example4preprocessing.vtu -o forward.vtu -s')
     runfile('vtu_trafo.py', args='-i forward.vtu -o backward.vtu -r')
 MANUAL SLICE DEFINITION
     runfile('vtu_trafo.py', args='-i example4preprocessing.vtu -o forward.vtu -x 9200 -X 18000 -y 9000 -Y 20000')
     runfile('vtu_trafo.py', args='-i forward.vtu -o backward.vtu -x 9200 -X 18000 -y 9000 -Y 20000 -r')
-EXAMPLE FOR TRAFO OF RESULTS   
+EXAMPLE FOR TRAFO OF RESULTS
     runfile('vtu_trafo.py', args='-i example4postprocessing.vtu -o backward.vtu -x 9200 -X 18000 -y 9000 -Y 20000 -r')
-   
+
 TODO
     implement for further THM result types, possibly for input data too
         done Darcy velocity (point data)
@@ -112,10 +112,10 @@ mutex.add_argument(
         help="store orginal data (only for direct trafo)"
     )
 
-parser.add_argument('-v', '--version', action='version', version=vtutrafo_version) 
+parser.add_argument('-v', '--version', action='version', version=vtutrafo_version)
 args = parser.parse_args()
 
-# check command line arguments 
+# check command line arguments
 store_flag = args.store
 reverse_flag = args.reverse
 start_x = args.start_x
@@ -130,7 +130,7 @@ auto_detect = (start_x == end_x) and (start_y == end_y)
 # Read file
 reader = vtk.vtkXMLUnstructuredGridReader()
 reader.SetFileName(inputfile)
-reader.Update()  
+reader.Update()
 vtk_mesh = reader.GetOutput()
 vtk_pdata = vtk_mesh.GetPointData()
 vtk_cdata = vtk_mesh.GetCellData()
@@ -139,17 +139,17 @@ vtk_points = vtk_mesh.GetPoints()
 print("{} Points".format(vtk_mesh.GetNumberOfPoints()))
 pdata_array_names = []
 for i in range(vtk_pdata.GetNumberOfArrays()):
-    pdata_array_names.append( vtk_pdata.GetArrayName(i) )
-print("Point Data:", pdata_array_names)    
+    pdata_array_names.append(vtk_pdata.GetArrayName(i))
+print("Point Data:", pdata_array_names)
 
 print("{} Cells".format(vtk_mesh.GetNumberOfCells()))
 cdata_array_names = []
 for i in range(vtk_cdata.GetNumberOfArrays()):
-    cdata_array_names.append( vtk_cdata.GetArrayName(i) )
-print("Cell Data: ", cdata_array_names)    
+    cdata_array_names.append(vtk_cdata.GetArrayName(i))
+print("Cell Data: ", cdata_array_names)
 
 points = vtk_to_numpy(vtk_points.GetData())
-N_points = len(points)    
+N_points = len(points)
 
 # store original points
 if args.store:
@@ -166,13 +166,13 @@ if (ORG_X in pdata_array_names) and (ORG_Y in pdata_array_names) and (ORG_Z in p
     org_points = np.stack( [np.array(vtk_to_numpy(vtk_pdata.GetArray(ORG_X))),
                             np.array(vtk_to_numpy(vtk_pdata.GetArray(ORG_Y))),
                             np.array(vtk_to_numpy(vtk_pdata.GetArray(ORG_Z)))], axis=-1 )
-    
+
 
 ###   DEFINE BASES   ###
 global_ex = np.array([1, 0, 0])   # 3D coordinate system
 global_ey = np.array([0, 1, 0])
 global_ez = np.array([0, 0, 1])
-global_base = np.matrix([global_ex, global_ey, global_ez])# note, base as row_vectors 
+global_base = np.matrix([global_ex, global_ey, global_ez])# note, base as row_vectors
 
 # construct base from given slice direction or automatically if not given
 if auto_detect:
@@ -180,26 +180,26 @@ if auto_detect:
         if stored_data_found:
             print("reading original points from " + inputfile)
             # vectors from point with minimum of a coordinate to point with maximum
-            dP = np.array([ org_points[np.argmax(org_points[:,0])] - org_points[np.argmin(org_points[:,0])], 
-                            org_points[np.argmax(org_points[:,1])] - org_points[np.argmin(org_points[:,1])], 
-                            org_points[np.argmax(org_points[:,2])] - org_points[np.argmin(org_points[:,2])] ])   
+            dP = np.array([ org_points[np.argmax(org_points[:,0])] - org_points[np.argmin(org_points[:,0])],
+                            org_points[np.argmax(org_points[:,1])] - org_points[np.argmin(org_points[:,1])],
+                            org_points[np.argmax(org_points[:,2])] - org_points[np.argmin(org_points[:,2])] ])
         else:
             print("WARNING! Automatic detection for reverse trafo, but no stored data found. No output written.")
             sys.exit()
-    else:  
+    else:
         print("detecting slice orientation automatically")
     # vectors from point with minimum of a coordinate to point with maximum
-        dP = np.array([ points[np.argmax(points[:,0])] - points[np.argmin(points[:,0])], 
-                        points[np.argmax(points[:,1])] - points[np.argmin(points[:,1])], 
-                        points[np.argmax(points[:,2])] - points[np.argmin(points[:,2])] ])  
-    dPnorm = np.linalg.norm(dP, axis=-1) 
+        dP = np.array([ points[np.argmax(points[:,0])] - points[np.argmin(points[:,0])],
+                        points[np.argmax(points[:,1])] - points[np.argmin(points[:,1])],
+                        points[np.argmax(points[:,2])] - points[np.argmin(points[:,2])] ])
+    dPnorm = np.linalg.norm(dP, axis=-1)
     dP_sorted = dP[np.argsort(dPnorm)]
     local_Ex = dP_sorted[-1]   # 2D coordinate system
     local_EY = dP_sorted[-2]
 else:
-    local_Ex = np.array([end_x-start_x, end_y-start_y, 0.0])   # 2D coordinate system    
-    local_EY = np.array([0.0, 0.0, 1.0])   
-    
+    local_Ex = np.array([end_x-start_x, end_y-start_y, 0.0])   # 2D coordinate system
+    local_EY = np.array([0.0, 0.0, 1.0])
+
 local_ex = local_Ex / np.linalg.norm(local_Ex)    # unit vector
 local_Ey = local_EY - np.dot(local_EY, local_ex)*local_ex   # make orthogonal
 local_ey = local_Ey / np.linalg.norm(local_Ey)   # unit vector
@@ -213,7 +213,7 @@ local_base = np.matrix([local_ex, local_ey, local_ez])
 #myR = R.from_matrix(myRmatrix)
 
 # scipy way
-spR, spMeanError = R.align_vectors(global_base, local_base)   
+spR, spMeanError = R.align_vectors(global_base, local_base)
 #spRmatrix = spR.as_matrix()
 
 
@@ -224,10 +224,10 @@ if reverse_flag and auto_detect:
     points = org_points
 else:
     if (args.set_2D_z is not None) and reverse_flag:
-        points[:,2] = args.set_2D_z   # set z coordinate in 2D, i.e. before trafo    
-    points = spR.apply(points, inverse=reverse_flag) 
+        points[:,2] = args.set_2D_z   # set z coordinate in 2D, i.e. before trafo
+    points = spR.apply(points, inverse=reverse_flag)
     if (args.set_2D_z is not None) and not reverse_flag:
-        points[:,2] = args.set_2D_z   # set z coordinate in 2D, i.e. after trafo 
+        points[:,2] = args.set_2D_z   # set z coordinate in 2D, i.e. after trafo
 vtk_points.SetData(numpy_to_vtk(points))
 
 if stored_data_found:
@@ -242,21 +242,22 @@ for pdata_array_name in pdata_array_names:
         pdata_array = vtk_to_numpy(vtk_pdata_array)
         rows, cols = pdata_array.shape
         if cols == 2:   # append zero z-component
-            pdata_array = spR.apply( np.concatenate((pdata_array, np.zeros((rows,1))), axis=1), inverse=reverse_flag) 
-            print(pdata_array_name + " augmented (2D to 3D) and transformed")      
+            pdata_array = spR.apply( np.concatenate((pdata_array, np.zeros((rows,1))), axis=1),
+                    inverse=reverse_flag)
+            print(pdata_array_name + " augmented (2D to 3D) and transformed")
             vtk_pdata.RemoveArray(pdata_array_name)
             r = vtk_pdata.AddArray(numpy_to_vtk(pdata_array))
             vtk_pdata.GetArray(r).SetName(pdata_array_name)
         elif cols == 3:
-            pdata_array = spR.apply(pdata_array, inverse=reverse_flag) 
-            print(pdata_array_name + " transformed")    
+            pdata_array = spR.apply(pdata_array, inverse=reverse_flag)
+            print(pdata_array_name + " transformed")
             vtk_pdata.RemoveArray(pdata_array_name)
             r = vtk_pdata.AddArray(numpy_to_vtk(pdata_array))
             vtk_pdata.GetArray(r).SetName(pdata_array_name)
         else:
             print("Not a vector: ", pdata_array_name)
 
-# write file    
+# write file
 writer = vtk.vtkXMLUnstructuredGridWriter()
 writer.SetFileName(outputfile)
 writer.SetInputData(vtk_mesh)
