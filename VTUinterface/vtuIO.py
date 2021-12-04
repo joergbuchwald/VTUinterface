719b6386916085cfebd3b71e4dd67040cf357f25
joergbuchwald <joerg.buchwald@ufz.de>
2021-12-04 20:53:21 +0100

    add summary dataframe for header and data array information

--- VTUinterface/vtuIO.py
+++ VTUinterface/vtuIO.py
@@ -81,6 +81,37 @@ class VTUIO:
         self.interpolation_backend = interpolation_backend
 
     @property
+    def header(self):
+        header_dict = {"N Cells": [str(len(self.cell_center_points))], "N Points": [str(len(self.points))],
+                "N Cell Arrays": [len(self.get_cell_field_names())],
+                "N Point Arrays": [len(self.get_point_field_names())],
+                "X Bounds": [str(np.min(self.points[:,0])) + ", " + str(np.max(self.points[:,0]))]}
+        if self.dim > 1:
+            header_dict["Y Bounds"] = [str(np.min(self.points[:,1]))+" "+str(np.max(self.points[:,1]))]
+        if self.dim > 2:
+            header_dict["Z Bounds"] = [str(np.min(self.points[:,2]))+" "+str(np.max(self.points[:,2]))]
+        df = pd.DataFrame(header_dict).T
+        return df.rename({0:"Information"}, axis='columns')
+
+    @property
+    def data_arrays(self):
+        pf_names = self.get_point_field_names()
+        cf_names = self.get_cell_field_names()
+        data_array_dict = {}
+        for name in pf_names:
+            field = self.get_point_field(name)
+            if field.ndim == 1:
+                components = 1
+            else:
+                components = field.shape[1]
+            data_array_dict[name] = ["point", components, np.min(field), np.max(field)]
+        for name in cf_names:
+            field = self.get_cell_field(name)
+            data_array_dict[name] = ["cell", components, np.min(field), np.max(field)]
+        df = pd.DataFrame(data_array_dict).T
+        return df.rename({0:"type", 1: "components", 2: "Min", 3: "Max"}, axis='columns')
+
+    @property
     def cell_center_points(self):
         """
         Method for obtaining cell center points
